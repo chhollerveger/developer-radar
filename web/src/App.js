c7c65801d351b9950c035da5619bf57f271c1441
Carlos Henrique Hollerveger <chhollerveger@gmail.com>
2020-01-16 23:53:02 -0300

    Added update web

--- web/src/App.js
+++ web/src/App.js
@@ -1,97 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
+
 function App() {
+const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  };
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-        <div className="input-group">
-          <div class="input-block">
-            <label htmlFor="latitude">Latitude</label>
-            <input name="latitude" id="latitude" required/>
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="longitude">Longitude</label>
-            <input name="longitude" id="longitude" required/>
-          </div>
-
-
-        </div> 
-          <button type="submit">Salvar</button>
-        </form>
-
-
+        <DevForm onSubmit={handAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/54322468?s=460&v=4" alt="Carlos Henrique" />
-              <div className="user-info">
-                <strong>Carlos Henrique</strong>
-                <span>Node.js, React, React Native</span>
-              </div>
-            </header>
-            <p>Bio</p>
-            <a href="https://github.com/chhollerveger">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/54322468?s=460&v=4" alt="Carlos Henrique" />
-              <div className="user-info">
-                <strong>Carlos Henrique</strong>
-                <span>Node.js, React, React Native</span>
-              </div>
-            </header>
-            <p>Bio</p>
-            <a href="https://github.com/chhollerveger">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/54322468?s=460&v=4" alt="Carlos Henrique" />
-              <div className="user-info">
-                <strong>Carlos Henrique</strong>
-                <span>Node.js, React, React Native</span>
-              </div>
-            </header>
-            <p>Bio</p>
-            <a href="https://github.com/chhollerveger">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/54322468?s=460&v=4" alt="Carlos Henrique" />
-              <div className="user-info">
-                <strong>Carlos Henrique</strong>
-                <span>Node.js, React, React Native</span>
-              </div>
-            </header>
-            <p>Bio</p>
-            <a href="https://github.com/chhollerveger">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
-
-
       </main>
-
-
     </div>
   );
-}
+};
 
 export default App;
